633d93c40ec6d5608ef338f1184759f5dad10b5f
Justin Millar <justinjmillar@gmail.com>
2016-11-18 17:48:20 -0500

    "Added function to calculate GC content, clarified variable names"

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -2,5 +2,19 @@
 ## This is an explaination about the purpose of this script
 ####
 
-data <- read.csv("data/houseelf-earlength-dna-data_1.csv", header = T)
-data
+library(stringr)
+
+ear_length_dat <- read.csv("data/houseelf-earlength-dna-data_1.csv", header = T)
+ear_length_dat
+
+get_gc_content <- function(sequence){
+  # Calculate the GC content for a sequence of base-pairs
+  sequence <- str_to_lower(sequence)
+  Gs <- str_count(sequence, "g")
+  Cs <- str_count(sequence, "c")
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100
+  return(gc_content)
+  
+}
+
+get_gc_content(ear_length_dat$dnaseq)
